919b5194a9b7d9c05706faaa1e90346a016bb150
Ben-Hadji <benhadji269@gmail.com>
2024-03-22 16:47:15 +0100

    let's go

--- app.js
+++ app.js
@@ -1,14 +1,104 @@
+require('dotenv').config();
 const express = require('express');
+const { Pool } = require('pg');
+
 const app = express();
+app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Ceci est une réponse à une requête GET');
+// Configure PostgreSQL connection
+const pool = new Pool({
+  host: "dpg-cnuor7md3nmc73abjrf0-a.oregon-postgres.render.com",
+  user: "eleves_user",
+  password: "FCwlX5t1Y2QF2JLyf6RIgEVX7xdZ1uHq",
+  port: 5432,
+  database: "eleves",
+  ssl: { rejectUnauthorized: false }
 });
 
-app.post('/', (req, res) => {
-    res.send('Ceci est une réponse à une requête POST');
+// Create 'eleves' table
+const createTableQuery = `
+  CREATE TABLE IF NOT EXISTS eleves (
+  id SERIAL PRIMARY KEY,
+  nom VARCHAR(100),
+  prenom VARCHAR(100),
+  age INT
+  )
+`;
+
+pool.query(createTableQuery, (err, result) => {
+  if (err) {
+    console.error('Error creating table:', err);
+    return;
+  }
+  console.log('Table \'eleves\' created.');
+
+  // Check if the table is empty
+  pool.query('SELECT COUNT(*) FROM eleves', (err, result) => {
+    if (err) {
+      console.error('Error counting rows:', err);
+      return;
+    }
+
+    // Insert initial data if the table is empty
+    if (parseInt(result.rows[0].count) === 0) {
+      const initialData = [
+        ['Jean', 'Dupont', 15],
+        ['Marie', 'Durand', 16],
+        ['Paul', 'Martin', 14],
+        ['Julie', 'Lefevre', 15]
+      ];
+      const insertInitialDataQuery = `INSERT INTO eleves (nom, prenom, age) VALUES ($1, $2, $3)`;
+      initialData.forEach(data => {
+        pool.query(insertInitialDataQuery, data, (err, result) => {
+          if (err) {
+            console.error('Error inserting initial data:', err);
+            return;
+          }
+        });
+      });
+      console.log('Initial data inserted.');
+    }
+  });
 });
 
-app.listen(3000, () => {
-    console.log('Le serveur est en écoute sur le port 3000');
+// Service POST pour ajouter un nouvel élève
+app.post('/eleves', (req, res) => {
+  const { nom, prenom, age } = req.body;
+  const insertEleveQuery = `INSERT INTO eleves (nom, prenom, age) VALUES ($1, $2, $3)`;
+  pool.query(insertEleveQuery, [nom, prenom, age], (err, result) => {
+    if (err) {
+      console.error('Error inserting new student:', err);
+      res.status(500).send('Error inserting new student');
+    } else {
+      console.log('New student inserted.');
+      res.status(201).send('New student inserted.');
+    }
+  });
+});
+
+// Service GET pour récupérer tous les élèves existants
+app.get('/eleves', (req, res) => {
+  const selectAllElevesQuery = 'SELECT * FROM eleves';
+  pool.query(selectAllElevesQuery, (err, results) => {
+    if (err) {
+      console.error('Error retrieving students:', err);
+      res.status(500).send('Error retrieving students');
+    } else {
+      // Format the results before sending
+      const formattedResults = results.rows.map(eleve => {
+        return {
+          id: eleve.id,
+          nom: eleve.nom,
+          prenom: eleve.prenom,
+          age: eleve.age
+        };
+      });
+      res.status(200).json(formattedResults);
+    }
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
 });
--- main.tf
+++ main.tf
@@ -3,15 +3,15 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
+  name     = "ben-resources"
   location = "West Europe"
 }
 
 resource "azurerm_kubernetes_cluster" "example" {
-  name                = "example-aks"
+  name                = "ben-aks"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
-  dns_prefix          = "exampleaks"
+  dns_prefix          = "benaks"
 
   default_node_pool {
     name       = "default"
